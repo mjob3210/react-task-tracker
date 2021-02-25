94e44d96e82d4a546b861ffbd143cdc3da977e0c
mjob3210 <mjob321@gmail.com>
2021-02-25 01:44:58 -0800

    Initialize repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  const name = 'Job'
+  return (
+    <div className="container">
+      <h1>Hello From React</h1>
+      <h2>Hello {name}</h2>
+    </div>
+  );
+}
+
+export default App;
